3105052ee8d756cdbdbedcccc4230ead55fc25be
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2022-01-06 11:34:52 -0300

    Add project files.

---
+++ src/MyApi/Program.cs
@@ -0,0 +1,40 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () =>
+{
+return Results.Ok("Hello World");
+});
+
+app.MapGet("/name/{nome}", (string nome) =>
+{
+return Results.Ok($"Hello {nome}");
+});
+
+app.MapPost("/", (User user) =>
+{
+return Results.Ok(user);
+});
+
+app.Run();
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+}
