1795e624ac0c08b3e42f89d8d43300f0acd6203a
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2022-04-04 09:55:09 -0300

    Added the home controller

---
+++ src/TodoMVC/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoMVC.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet()]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
