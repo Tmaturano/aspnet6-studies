527f8fb7d36fcde50a2a402ed2ccb8c9b5ff0f5f
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2022-04-04 09:43:22 -0300

    adding new MVC project and sql initialization with entity framework

---
+++ src/TodoMVC/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoMVC.Models;
+
+namespace TodoMVC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+            => optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
