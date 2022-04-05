eaa5fce5d091417705d0124a3d79be27dfa16e35
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2022-04-05 18:04:09 -0300

    added validations

--- src/Blog/Dtos/CategoryDto.cs
+++ src/Blog/Dtos/CategoryDto.cs
@@ -1,8 +1,15 @@
-﻿namespace Blog.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Dtos
 {
     public abstract class CategoryDto
     {
+        [Required]
+        [MinLength(3)]
         public string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
         public string Slug { get; set; }
     }
 }
