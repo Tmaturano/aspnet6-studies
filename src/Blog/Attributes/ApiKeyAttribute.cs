a0fd6e2ebec04eba6cc21fff2f5894724fb2f4b6
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2022-04-12 14:37:09 -0300

    using api_key for integrations

---
+++ src/Blog/Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Blog.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!context.HttpContext.Request.Query.TryGetValue(Configuration.ApiKeyName, out var extractedApiKey))
+        {
+            context.Result = new ContentResult
+            {
+                StatusCode = 401,
+                Content = "Api Key not found"
+            };
+
+            return;
+        }
+
+        if (!Configuration.ApiKey.Equals(extractedApiKey))
+        {
+            context.Result = new ContentResult
+            {
+                StatusCode = 403,
+                Content = "Access not authorized"
+            };
+
+            return;
+        }
+
+        await next();
+    }
+}
